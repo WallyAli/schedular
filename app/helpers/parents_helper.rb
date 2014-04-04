9947a8c7aabcdf9b278c4f2300f33776e7c2af9c
wallyali <abdishwak@gmail.com>
2014-04-03 19:46:02 -0500

    updated forms

--- app/helpers/parents_helper.rb
+++ app/helpers/parents_helper.rb
@@ -1,8 +1,3 @@
 module ParentsHelper
 
-	def setup_parent(parent)
-		parent.children ||= Child.new
-	    3.times {parent.children.build}
-	    parent
-    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Scheduler::Application.routes.draw do
+  
   resources :days
+  resources :parents, shallow: true do 
+    resources :children
+  end
 
-  resources :children
-
-  resources :parents
+  root 'parents#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
