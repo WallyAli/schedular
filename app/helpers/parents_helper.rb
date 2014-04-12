a29983bba8a928ab9f229277e9f4ffc29f9d754c
wallyali <abdishwak@gmail.com>
2014-04-12 01:54:09 -0500

    add table-border to schedules show view

--- app/helpers/parents_helper.rb
+++ app/helpers/parents_helper.rb
@@ -1,3 +1,6 @@
 module ParentsHelper
 
+	def parent_full_name(parent)
+		[parent.first_name, parent.last_name].join(' ')
+	end
 end
--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,3 +1,4 @@
 class Schedule < ActiveRecord::Base
 	has_and_belongs_to_many :children
+	has_many :parents, through: :children
 end
