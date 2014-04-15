187bfae5b67f1793f69ffd2d6a53299cbf0d2c1e
wallyali <abdishwak@gmail.com>
2014-04-15 03:04:33 -0500

    add date_format helper

--- app/helpers/parents_helper.rb
+++ app/helpers/parents_helper.rb
@@ -1,6 +1,11 @@
 module ParentsHelper
+	require 'date'
 
 	def parent_full_name(parent)
 		[parent.first_name, parent.last_name].join(' ')
+	end
+
+	def date_format(date)
+		Date.parse(date).strftime("%m/%d/%Y")
 	end
 end
