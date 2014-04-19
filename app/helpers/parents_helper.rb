b1a1bf6ee7dbd7b7dae1ebf1ec0eb593dca826e8
wallyali <abdishwak@gmail.com>
2014-04-19 02:53:28 -0500

    update views

--- app/helpers/parents_helper.rb
+++ app/helpers/parents_helper.rb
@@ -1,8 +1,12 @@
 module ParentsHelper
-	require 'date'
+	ActionView::Helpers::NumberHelper
 
 	def parent_full_name(parent)
 		[parent.first_name, parent.last_name].join(' ')
+	end
+
+	def phone_format(phone)
+		number_to_phone(phone)
 	end
 
 end
