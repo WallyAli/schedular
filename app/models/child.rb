01439dc418b7af9ac48f655a74f674f66bfa7b21
wallyali <abdishwak@gmail.com>
2014-04-20 18:42:34 -0500

    add inverse_of validation to parent and child models. remove time_zone field from new attendance form

--- app/models/child.rb
+++ app/models/child.rb
@@ -1,6 +1,6 @@
 class Child < ActiveRecord::Base
 
-	belongs_to :parent
+	belongs_to :parent, inverse_of: :children
 	validates_presence_of :first_name
 	validates_presence_of :last_name
 
--- app/models/parent.rb
+++ app/models/parent.rb
@@ -1,5 +1,5 @@
 class Parent < ActiveRecord::Base
-	has_many :children
+	has_many :children, :inverse_of => :parent
 	has_many :schedules, through: :children
 
 	validates_presence_of :first_name
