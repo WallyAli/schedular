e52b14d7255f45da1ee73e71c909e997a7b3c4ff
wallyali <abdishwak@gmail.com>
2014-04-20 01:57:25 -0500

    add age column by calculating from birthday

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,9 +3,4 @@ module ApplicationHelper
 	def time_format(time)
 	  time.strftime("%I:%M %p") unless time.blank?
 	end
-
-	def formatted_time
-		Time::DATE_FORMATS[:non_military_time] = "%I:%M %p"
-	end
-	
 end
--- app/helpers/children_helper.rb
+++ app/helpers/children_helper.rb
@@ -4,9 +4,15 @@ module ChildrenHelper
 		[child.first_name, child.last_name].join(' ')
 	end
 
-  def age(dob)
-    now = DateTime.now.utc.to_date
-    now.year - dob.year - ((now.month > dob.month || (now.month == dob.month && now.day >= dob.day)) ? 0 : 1)
-  end
+	def age(dob)
+	  now = DateTime.now.to_date
+	  now.year - string_to_date(dob).year - 
+	  	((now.month > string_to_date(dob).month || 
+	  		(now.month == string_to_date(dob).month && 
+	  			now.day >= string_to_date(dob).day)) ? 0 : 1)
+	end
 
+	def string_to_date(dob)
+		Date.strptime(dob, "%m/%d/%Y")
+	end
 end
