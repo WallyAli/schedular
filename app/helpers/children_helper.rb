2a418e35580998cacdaca31621a3b52b3df8e96b
wallyali <abdishwak@gmail.com>
2014-04-20 02:26:19 -0500

    fixed age helper

--- app/helpers/children_helper.rb
+++ app/helpers/children_helper.rb
@@ -5,14 +5,16 @@ module ChildrenHelper
 	end
 
 	def age(dob)
-	  now = DateTime.now.to_date
-	  now.year - string_to_date(dob).year - 
-	  	((now.month > string_to_date(dob).month || 
-	  		(now.month == string_to_date(dob).month && 
-	  			now.day >= string_to_date(dob).day)) ? 0 : 1)
+		unless dob.blank?
+		  now = DateTime.now.to_date
+		  now.year - string_to_date(dob).year - 
+		  	((now.month > string_to_date(dob).month || 
+		  		(now.month == string_to_date(dob).month && 
+		  			now.day >= string_to_date(dob).day)) ? 0 : 1)
+		end
 	end
 
 	def string_to_date(dob)
-		Date.strptime(dob, "%m/%d/%Y")
+		Date.strptime(dob, "%m/%d/%Y") unless dob.blank?
 	end
 end
