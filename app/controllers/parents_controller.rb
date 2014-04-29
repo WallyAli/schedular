35121a888c88c4058e245620beaf0a6f5c10e783
wallyali <abdishwak@gmail.com>
2014-04-28 21:12:07 -0500

    refactore parents_controller

--- app/controllers/parents_controller.rb
+++ app/controllers/parents_controller.rb
@@ -1,59 +1,46 @@
 class ParentsController < ApplicationController
   before_action :set_parent, only: [:show, :edit, :update, :destroy]
 
-  # GET /parents
-  # GET /parents.json
   def index
     @parents = Parent.all
   end
 
-  # GET /parents/1
-  # GET /parents/1.json
   def show
   end
 
-  # GET /parents/new
   def new
     @parent = Parent.new
     3.times { @parent.children.build }
   end
 
-  # GET /parents/1/edit
   def edit
   end
 
-  # POST /parents
-  # POST /parents.json
   def create
     @parent = Parent.new(parent_params)
-
     respond_to do |format|
       if @parent.save
         format.html { redirect_to @parent, notice: 'Parent was successfully created.' }
         format.json { render action: 'show', status: :created, location: @parent }
       else
-        format.html { render action: 'new' }
+        format.html { render :new }
         format.json { render json: @parent.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # PATCH/PUT /parents/1
-  # PATCH/PUT /parents/1.json
   def update
     respond_to do |format|
       if @parent.update(parent_params)
         format.html { redirect_to @parent, notice: 'Parent was successfully updated.' }
         format.json { head :no_content }
       else
-        format.html { render action: 'edit' }
+        format.html { render :edit }
         format.json { render json: @parent.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # DELETE /parents/1
-  # DELETE /parents/1.json
   def destroy
     @parent.destroy
     respond_to do |format|
@@ -63,17 +50,19 @@ class ParentsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+
     def set_parent
       @parent = Parent.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def parent_params
-      params.require(:parent).permit(:case_number, :first_name, :middle_name, :last_name, 
-                                      :address, :phone, :city, :state, :zipcode, :email, 
+      params.require(:parent).permit(:case_number, :first_name, 
+                                     :middle_name, :last_name, 
+                                      :address, :phone, :city, 
+                                      :state, :zipcode, :email, 
                                       children_attributes: [ 
-                                      :id, :first_name, :last_name, :birthday, :gender,
+                                      :id, :first_name, :last_name, 
+                                      :birthday, :gender,
                                       :_destroy, {:schedule_ids => []}],
                                       schedule_attributes: [:id, :name])
     end
